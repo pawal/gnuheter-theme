d24a0d5a93cb1a6b131f5254c387d8a22effbb83
Patrik Wallström <pawal@blipp.com>
2010-09-10 21:51:08 +0200

    Setting an id for entrygroup, hopefully closes #1

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
     <span class="datum"><?php the_time('Y-m-d') ?> av <?php the_author_nickname(); ?></span><br />
     <a href="<?php the_syndication_source_link(); ?>"><?php the_syndication_source(); ?></a>
     <?php } ?></h3>
-  <div class="entrygroup"><h4>
-    <a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h4>
+  <div class="entrygroup" id="<?php the_permalink(); ?>"><h4>
+    <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h4>
     <div class="entry"><div><?php the_content('Read the rest of this entry &raquo;'); ?></div>
     </div></div></div>
   <?php endwhile; ?>
